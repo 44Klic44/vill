c098d906ae4e1af87ab04031adc81b7a41b51878
ROMAN TYSACHNYY <stance_srt@mail.ru>
2026-02-07 08:47:01 +0300

    Инициализация проекта/установка библиотек redux-toolkit/react-router-dom/Tailwind/Headless/sonner/reharts/react-icon/moment/clsx 2. Создал компоненты для страниц приложения / добавил стили основные в файл index.css Создал компоненты button.jsx и texbox.jsx использую их на странице login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Navigate, Outlet, useLocation, replace } from 'react-router-dom'
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+import Users from "./pages/Users"
+import Tasks from "./pages/Tasks"
+import Trash from "./pages/Trash"
+import TaskDetails from "./pages/TaskDetails"
+import { Toaster } from "sonner"
+
+
+function Layout(){
+  const user =""
+  const location = useLocation()
+  return user ? (
+  <div className="w-full h-screen flex flex-col md:flex-row">
+<div className="w-1/5 b-screen bg-white sticky top-0 hidden md:block">
+ {/* <Sidebar/> */}
+</div>
+
+
+ {/* <MobileSidebar/> */}
+
+<div className="flex-1 overflow-y-auto"> 
+{/* <Navbar/> */}
+</div>
+
+<div className="p-4 2x1:px-10">
+  {/* <Outlet/> */}
+</div>
+
+
+  </div>
+  ):(
+    <Navigate to="/log-in" state={{from: location}} replace/>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-        <div className="min-h-screen bg-gray-100 p-8">
-      <h1 className="text-3xl font-bold text-blue-600 mb-4">
-        Tailwind CSS работает! 🎉
-      </h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Тестовая кнопка
-      </button>
-      <div className="mt-4 p-64 bg-green-100 border border-green-400 rounded">
-        Если видишь стили — всё настроено правильноапапапапап!
-      </div>
-    </div>
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+       
+       <Routes>
+        <Route element={<Layout/>}>
+              <Route path="/" element={< Navigate to="/dashbord" /> }/>
+              <Route path="/dashboard" element={< Dashboard /> }/>
+              <Route path="/tasks" element={< Tasks /> }/>
+              <Route path="/completed/:status" element={< Tasks /> }/>
+              <Route path="/in-progress/:status" element={< Tasks /> }/>
+              <Route path="/todo/:status" element={< Tasks /> }/>
+              <Route path="/team" element={< Users/> }/>
+              <Route path="/trashed" element={< Trash/> }/>
+              <Route path="/task/:id" element={< TaskDetails/> }/>
+        </Route>
+
+
+          <Route path="/log-in" element={< Login/> }/>
+       </Routes>
+       <Toaster richColors/>
+      </main>
   )
 }
 
 export default App
+
