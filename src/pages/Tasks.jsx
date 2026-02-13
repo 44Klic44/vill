489d3c6c540a4013e650f2b5ae53cd3fb93d63cf
Roman <stance_srt@mail.ru>
2026-02-13 17:38:32 +0300

    Работаем с страницей задач. Добавил компонент Title и Loader. В компоненьте Task добавил кнопку для создания задач

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
+import Title from '../components/Title';
+import Loading from '../components/Loader';
+import React, { useState } from 'react'
+import { FaList } from 'react-icons/fa';
+import { MdGridView } from 'react-icons/md';
+import { useParams } from 'react-router-dom';
+import  Button  from '../components/Button';
+import { IoMdAdd } from 'react-icons/io';
+
+
+const TABS = [
+  { title: "Board View", icon: <MdGridView /> },
+  { title: "List View", icon: <FaList /> },
+];
+
+const TASK_TYPE = {
+  todo: "bg-blue-600",
+  "in progress": "bg-yellow-600",
+  completed: "bg-green-600",
+};
 
 const Tasks = () => {
-  return (
-    <div>
-      Tasks
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, eligendi voluptate
-    </div>
-  )
-}
+  const params = useParams();
+
+  const [selected, setSelected] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const status = params?.status || "";
+
+
+
+
+  return loading ? (
+    <div className='py-10'>
+      <Loading />
+    </div> 
+    ) : (
+      <div className='w-full'> 
+            <div className='flex items-center justify-between mb-4'>
+        <Title   title={status ? `${status} Tasks` : "Tasks"} />
+
+      {!status && (
+          <Button
+            onClick={() => setOpen(true)}
+            label='Create Task'
+            icon={<IoMdAdd className='text-lg' />}
+            className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5'
+          />
+        )}
+      
+      </div>
+      </div>
+    );
+};
 
 export default Tasks
