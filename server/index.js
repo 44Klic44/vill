eb0fc19e2747f41c626b0890c393508ff49f0610
ROMAN TYSACHNYY <stance_srt@mail.ru>
2026-02-20 20:51:24 +0300

    Проверить работу сервера

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
-
-import { dbConnection } from "./utils/index.js";
+import routes from "../server/utils/index.js"
+import dbConnection from "./utils/index.js";
+import { errorHandler, routeNotFound } from "./middlewares/errorMiddleware.js";
 
 
 dotenv.config();
@@ -12,5 +13,31 @@ dotenv.config();
 dbConnection();
 
 const PORT = process.env.PORT || 5000;
+// const routes =""
+
+
 
 const app = express();
+
+
+app.use(
+  cors({
+    origin: ["https://mern-task-manager-app.netlify.app", "http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(morgan("dev"));
+app.use("/api", routes);
+
+app.use(routeNotFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
