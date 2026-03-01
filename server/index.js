b18757e935941f6c6c3076339cd92bf2a23977e7
ROMAN TYSACHNYY <stance_srt@mail.ru>
2026-03-01 09:10:59 +0300

    Готовим деплой

--- server/index.js
+++ server/index.js
@@ -1,46 +1,24 @@
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import morgan from "morgan";
-import routes from "./routes/index.js";
-import dbConnection from "./utils/index.js";
-import { errorHandler, routeNotFound } from "./middlewares/errorMiddleware.js";
-
-dotenv.config();
-
-dbConnection();
-
-const PORT = process.env.PORT || 5000;
-
-const app = express();
-
-app.use((req, res, next) => {
-  next();
-});
-
-
 app.use(
   cors({
-    origin: [
-      "https://mern-task-manager-app.netlify.app",
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "http://localhost:5174"   
-    ],
+    origin: function (origin, callback) {
+      // Разрешённые домены
+      const allowedOrigins = [
+        process.env.CLIENT_URL,
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:5174"
+      ].filter(Boolean); 
+
+  
+      if (!origin) return callback(null, true);
+      
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
 );
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(morgan("dev"));
-
-app.use("/api", routes);
-
-app.use(routeNotFound);
-app.use(errorHandler);
-
-app.listen(PORT, () => {});
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/vill/' : '/',
   server: {
-    port: 5174,       // фиксируем порт
-    strictPort: true, // если занят — не запускать другой
+    port: 5174,
+    strictPort: true,
   },
 })
