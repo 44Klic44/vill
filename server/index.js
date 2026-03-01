a4b8c7ae18657f89de15f7df59fac8ec125c84d2
ROMAN TYSACHNYY <stance_srt@mail.ru>
2026-03-01 10:22:21 +0300

    Fix: add missing app declaration

--- server/index.js
+++ server/index.js
@@ -1,17 +1,28 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import routes from "./routes/index.js";
+import dbConnection from "./utils/index.js";
+import { errorHandler, routeNotFound } from "./middlewares/errorMiddleware.js";
+
+dotenv.config();
+dbConnection();
+
+const app = express();
+
+
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Разрешённые домены
       const allowedOrigins = [
         process.env.CLIENT_URL,
         "http://localhost:3000",
         "http://localhost:3001",
         "http://localhost:5174"
-      ].filter(Boolean); 
-
-  
+      ].filter(Boolean);
       if (!origin) return callback(null, true);
-      
       if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
@@ -22,3 +33,16 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use("/api", routes);
+app.use(routeNotFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
